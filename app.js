7d2a54da4b81ff77ece445e3b279fe4638e1a059
Raketh <rakethpallepogu@gmail.com>
2024-08-14 12:31:19 +0530

    call back in javaScript

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
- btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked!!")
- })  
+function clickHandler() { 
+   console.log("clicked!!");
+};  
+
+ btnTranslate.addEventListener("click", clickHandler)  
+
+
